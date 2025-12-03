98d0853a187909c8fe8300c252652c71d8be3ffb
Rasmus <rasser1357@gmail.com>
2025-12-03 23:22:25 +0100

    ugly ahh code

--- 2025/1st_December/main.py
+++ 2025/1st_December/main.py
@@ -29,6 +29,32 @@ def part_1(data):
     print("part 1:", password)
 
 
+def part_2(data):
+    number = 50
+    password = 0
+
+    prev_num = number
+
+    for i in data:
+        direction = i[0]
+        distance = int(i[1:])
+
+        number += distance if direction == "R" else -distance
+
+        password += abs(number // 100)
+        number %= 100
+
+        if direction == "L":
+            if number == 0:
+                password += 1
+
+            if prev_num == 0:
+                password -= 1
+
+
+    prev_num = number
+
+    print("part 2:", password)
 
 
 if __name__ == "__main__":
