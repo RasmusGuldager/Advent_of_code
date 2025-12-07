953470419ac3f45fe84b3a7b719dcd5d62341bf0
Rasmus <rasser1357@gmail.com>
2025-12-07 15:00:46 +0100

    part 1 done

---
+++ 2025/4th_December/main.py
@@ -0,0 +1,90 @@
+def get_data(test=False):
+    if not test:
+        with open("data.txt", "r") as file:
+            data = file.readlines()
+    else:
+        with open("test_data.txt", "r") as file:
+            data = file.readlines()
+
+    data = [line.strip() for line in data]
+
+    return data
+
+
+class Spot:
+    def __init__(self, x, y, paper) -> None:
+        self.x: int = x
+        self.y: int = y
+        self.paper: bool = paper
+        self.neighbors: list[bool] = []
+        self.paper_neighbors: int = 0
+
+    def find_neighbors(self, grid):
+        directions = [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]
+
+        max_x = len(grid)
+        max_y = len(grid[0])
+
+        for dx, dy in directions:
+            nx = self.x + dx
+            ny = self.y + dy
+
+            if 0 <= nx < max_x and 0 <= ny < max_y:
+                self.neighbors.append(grid[ny][nx].paper)
+                self.paper_neighbors += grid[ny][nx].paper
+
+
+def create_grid(data):
+    grid = [[0 for _ in range(max_x)] for _ in range(max_y)]
+
+    # Create spots
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            grid[y][x] = Spot(x, y, data[y][x] == "@")
+
+    # Find neighbors
+    for y in range(max_y):
+        for x in range(max_x):
+            grid[y][x].find_neighbors(grid)
+
+    return grid
+
+
+def part_1(grid):
+    accessible_paper = 0
+
+    for y in range(max_y):
+        for x in range(max_x):
+            if grid[y][x].paper:
+                if grid[y][x].paper_neighbors < 4:
+                    accessible_paper += 1
+
+    print("Part 1:", accessible_paper)
+
+
+def part_2(grid):
+    pass
+
+
+if __name__ == "__main__":
+    data = get_data()
+
+    global max_x
+    max_x = len(data[0])
+
+    global max_y
+    max_y = len(data)
+
+    grid = create_grid(data)
+
+    part_1(grid)
+    part_2(grid)
