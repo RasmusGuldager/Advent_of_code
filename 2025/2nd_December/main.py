498308e2463d07bd94c9c1883059e017763c6f86
Rasmus <rasser1357@gmail.com>
2025-12-07 01:51:20 +0100

    done

--- 2025/2nd_December/main.py
+++ 2025/2nd_December/main.py
@@ -30,8 +30,38 @@ def part_1(data):
     print("Part 1:", invalid)
 
 
+def part_2(data):
+    invalid = set()
+
+    for id_range in data:
+        min = id_range[0]
+        max = id_range[1]
+        
+        for i in range(min, max+1):
+            length = len(str(i))
+
+            # Calculate possible pattern lengths
+            pattern_lengths = []
+            for j in range(1,length):
+                if length % j == 0:
+                    pattern_lengths.append(j)
+
+            temp = str(i)
+
+            candidate_patterns = []
+            for pattern_length in pattern_lengths:
+                candidate_patterns.append(temp[:pattern_length])
+            
+            for candidate_pattern in candidate_patterns:
+                if candidate_pattern*(length // len(candidate_pattern)) == temp:
+                    invalid.add(i)
+    
+    print("Part 2:", sum(invalid))
+
+
 
 if __name__ == "__main__":
     data = get_data()
 
     part_1(data)
+    part_2(data)
